2c98cd6427060904cc416f44524908b6758c7df3
XVI6 <matvey06996@gmail.com>
2016-11-08 23:33:28 +0100

    Add ReproductorManager(without 3)

--- src/main/java/paintingsjdbc/domain/Reproduktor.java
+++ src/main/java/paintingsjdbc/domain/Reproduktor.java
@@ -4,6 +4,8 @@ public class Reproduktor {
 	private long id;
 	
 	private String name;
+	private String country;
+	private String city;
 	private String adress;
 	private String telephone;
 	private String e_mail;
@@ -12,9 +14,11 @@ public class Reproduktor {
 		super();
 	}
 	
-	public Reproduktor(String name, String adress, String telephone, String e_mail){
+	public Reproduktor(String name, String country, String city, String adress, String telephone, String e_mail){
 		super();
 		this.name = name;
+		this.country = country;
+		this.city = city;
 		this.adress = adress;
 		this.telephone = telephone;
 		this.e_mail = e_mail;
@@ -35,6 +39,22 @@ public class Reproduktor {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
 	}
 
 	public String getAdress() {
